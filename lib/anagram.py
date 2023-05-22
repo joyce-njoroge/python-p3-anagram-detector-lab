993959e4d7279a9bc2f4af4b8758431db380e2a2
Joyce Njoroge <joycewambuku33@gmail.com>
2023-05-22 15:53:33 +0300

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+class Anagram:
+   def __init__(self, word):
+      self.word = word.lower()
+      
+
+   def match(self, words):
+       match = []
+       for word in words:
+           if len(word) == len(self.word) and word.lower() != self.word:
+                if sorted(word.lower()) == sorted(self.word):
+                   match.append(word)
+       return match   
+            
+word = Anagram("Word")  
+word = (['enlists', 'google', 'inlets', 'banana'])    
